7f263577361f5b0e6fad4dff7a45167972111655
Jonatas Brisotti <jalmeidabrisotti22@fed.idserve.net>
2022-10-17 11:54:12 +0200

    Initial Commit

---
+++ magn/magnApp.swift
@@ -0,0 +1,17 @@
+//
+//  magnApp.swift
+//  magn
+//
+//  Created by Jonatas Brisotti on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct magnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
